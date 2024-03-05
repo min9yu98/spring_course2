87116011206f9f3f47f6db45e01c786a507b9ee8
Min Gyu <kimmingyu9811@gmail.com>
2024-03-05 22:50:02 +0900

    feat: 회원 도메인 개발

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -3,5 +3,5 @@ package hello.core.member;
 public interface MemberRepository {
     void save(Member member);
 
-    Member finById(Long memberId);
+    Member findById(Long memberId);
 }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -13,7 +13,7 @@ public class MemoryMemberRepository implements MemberRepository {
     }
 
     @Override
-    public Member finById(Long memberId) {
+    public Member findById(Long memberId) {
         return store.get(memberId);
     }
 
